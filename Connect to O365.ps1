1ac0b3ba94075fed17e730d182eed9ac21b546d8
bsonnek <blairsonnek@hotmail.com>
2020-03-31 22:48:57 -0500

    Add files via upload

---
+++ Connect to O365.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy RemoteSigned
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+Get-IRMConfiguration
+Test-IRMConfiguration -Sender sbitech@smithschafer.com
+
+End Session
+Remove-PSSession $Session
